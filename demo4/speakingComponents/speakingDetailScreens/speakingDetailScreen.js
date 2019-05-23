5a8de4a1bc7af1e0354a4be5002535c09d9b28a5
maxakak1998 <50696626+maxakak1998@users.noreply.github.com>
2019-05-23 15:02:50 +0700

    Update speakingDetailScreen.js
    Vẫn chưa làm được record

--- demo4/speakingComponents/speakingDetailScreens/speakingDetailScreen.js
+++ demo4/speakingComponents/speakingDetailScreens/speakingDetailScreen.js
@@ -1,251 +1,339 @@
-import React,{Component} from 'react';
-import {View,StyleSheet,Alert,Text,Dimensions} from 'react-native';
-import faker from 'faker';
-import { Navigation } from 'react-native-navigation';
-import Slider from '@react-native-community/slider';
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  Alert,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+  Button
+} from "react-native";
+import faker from "faker";
+import { Navigation } from "react-native-navigation";
+import Slider from "@react-native-community/slider";
 
-import PlayIcon from 'react-native-vector-icons/AntDesign' //play
-import RecIcon from 'react-native-vector-icons/MaterialCommunityIcons' //record
-import CheckIcon from 'react-native-vector-icons/MaterialCommunityIcons' //speaker-wireless
+import PlayIcon from "react-native-vector-icons/AntDesign"; //play
+import RecIcon from "react-native-vector-icons/MaterialCommunityIcons"; //record
+import CheckIcon from "react-native-vector-icons/MaterialCommunityIcons"; //speaker-wireless
 
-import PauseIcon from 'react-native-vector-icons/MaterialCommunityIcons' //pause-circle
-import RecIconForPlaying from 'react-native-vector-icons/Foundation' //record
-import CheckIconForPlaying from 'react-native-vector-icons/MaterialCommunityIcons' //speaker-wireless
+import PauseIcon from "react-native-vector-icons/MaterialCommunityIcons"; //pause-circle
+import RecIconForPlaying from "react-native-vector-icons/Foundation"; //record
+import CheckIconForPlaying from "react-native-vector-icons/MaterialCommunityIcons"; //speaker-wireless
 // import Sound from 'react-native-sound';
-import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+import AudioRecorderPlayer from "react-native-audio-recorder-player";
 const sounder = new AudioRecorderPlayer();
-const screenWidth=Dimensions.get('screen').width;
-const screenHeight=Dimensions.get('screen').height;
+const screenWidth = Dimensions.get("screen").width;
+const screenHeight = Dimensions.get("screen").height;
+let currentPlayerTime = 0;
+class SliderComponent extends Component {
+  constructor(args) {
+    super(args);
 
-class SliderComponent extends Component{
-    constructor(args){
-        super(args);
-    }
-    render(){
-        return(
-            <View 
-            style={styles.sliderContainer}
-            >
+    this.state = {
+      isDrag: false
+    };
+  }
 
-            <Text
-            style={styles.currentPositionSecStyle}
-            >
-            0
-            </Text>
-                
-            <Slider
-            style={styles.sliderStyle}
-            />
+  _onSlidingComplete(value) {
+    sounder.seekToPlayer(value);
+  }
 
-            <Text
-            style={styles.durationPlayerTime}
-            >
-            4:10
-            </Text>
-            </View>
-            
-        );
-    }
+  render() {
+    return (
+      <View style={styles.sliderContainer}>
+        <Text style={styles.currentPositionSecStyle}>
+          {this.props.currentTime}
+        </Text>
 
+        <Slider
+          style={styles.sliderStyle}
+          minimumValue={0}
+          maximumValue={this.props.durationPlayerTimeInSecs}
+          value={this.props.currentTimePlayerInSecs}
+          onSlidingComplete={this._onSlidingComplete.bind(this)}
+        />
+
+        <Text style={styles.durationPlayerTime}>{this.props.durationTime}</Text>
+      </View>
+    );
+  }
 }
 
-class ButtonsComponent extends Component{
-    constructor(args){
-        super(args);
-        this.state={
-            isPlaying:false,
-            isRecording:false,
-            isReplaying:false
-        }
+class ButtonsComponent extends Component {
+  constructor(args) {
+    super(args);
+    this.state = {
+      isLoading: false,
+      isPlaying: false,
+      isRecording: false,
+      isReplaying: false,
+      currentTime: 0,
+      duration: 0,
+      durationTimeInSecs: 0,
+      currentTimePlayerInSecs: 0
+    };
+  }
+  componentDidMount() {
+    console.log("did mount");
+  }
+  componentWillMount() {
+    console.log("will mount");
+  }
 
+  componentWillUpdate() {
+    console.log("will update");
+    console.log("is loading:" + this.state.isLoading);
+    console.log("is playing: " + this.state.isPlaying);
+  }
+
+   componentDidUpdate(prevPro,prevState) {
+    if(this.state.isLoading!=prevState.isLoading){
+       console.log('different');
+        this.playAudio();  
     }
+   
+  }
 
-  
-    onStopPlayer=async()=>{
-        console.log('stop player');
-        sounder.stopPlayer();
-        sounder.removePlayBackListener();
-    }
+  onStopPlayer = async () => {
+    console.log("stop player");
+    sounder.stopPlayer();
+    sounder.removePlayBackListener();
+  };
 
-    onStartPlayer=async()=>{
-        console.log('start player');
-        const msg=await sounder.startPlayer('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
-        // sounder.addPlayBackListener((e)=>{
-        //     console.log(e);
-        // });
-        // console.log('msg: '+msg);
-    }
+  onStartPlayer = async () => {
+    console.log("onStartPlayer goes");
+    const msg =  await sounder.startPlayer(
+      "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+    );
+    console.log("onStartPlayer: go go go");
+    //set state for isLoading to false
+      this.setState({ isLoading: false });
+      console.log(msg);
+  };
 
+  playAudio() {
+     this.onStartPlayer();
+    // this.getCurrentTimePlayer();
+    // this.getDurationTime();
+    // this.getDurationTimeInSecs();
+    // this.getCurrentTimePlayerInSecs();
+  }
 
-    audioController(){
+  stopAudio() {
+    this.onStopPlayer();
+  }
 
-        if(this.state.isPlaying){
-            this.onStopPlayer();
-        }else
-        {
-           this.onStartPlayer();
-        }
+  getCurrentTimePlayer() {
+    sounder.addPlayBackListener(e => {
+      this.setState({
+        currentTime: sounder.mmss(Math.floor(e.current_position / 1000))
+      });
+    });
+  }
+  getCurrentTimePlayerInSecs() {
+    sounder.addPlayBackListener(e => {
+      this.setState({
+        currentTimePlayerInSecs: Math.floor(e.current_position / 1000)
+      });
+    });
+  }
+  getDurationTime() {
+    sounder.addPlayBackListener(e => {
+      this.setState({ duration: sounder.mmss(Math.floor(e.duration / 1000)) });
+    });
+  }
+  getDurationTimeInSecs() {
+    sounder.addPlayBackListener(e => {
+      this.setState({ durationTimeInSecs: Math.floor(e.duration / 1000) });
+      console.log("duration time in secs: " + this.state.durationTimeInSecs);
+    });
+  }
 
-    }
-    render(){
-        const pauseConst=<PauseIcon onPress={()=>{
-           this.setState({isPlaying:false});
-            this.audioController();
-        }} 
-        name='pause' size={35} color='red' style={styles.playIconStyle}/>;
+  render() {
+    console.log("render");
+    console.log('is loading at renderer is : '+this.state.isLoading);
+    console.log('is playing at renderer is : '+this.state.isPlaying);
+    const pauseConst = (
+      <PauseIcon
+        onPress={() => {
+          this.setState({ isPlaying: false });
+          console.log("set state in pause const");
+          this.stopAudio();
+        }}
+        name="pause"
+        size={35}
+        color="red"
+        style={styles.playIconStyle}
+      />
+    );
 
-        const playConst= <PlayIcon onPress={()=>{
-           this.setState({isPlaying:true});
-            this.audioController();
+    const loaderConst = <ActivityIndicator size="large" color="#0000ff" />;
 
-        }} 
-        name='play' size={35} color='black' style={styles.playIconStyle}/>;
+    const playConst = (
+      <PlayIcon
+        onPress={ () => {
+          this.setState({ isPlaying: true });
+          this.setState({ isLoading: true });
+          console.log("set state in playconst");
+          console.log("is playing at playConst: " + this.state.isPlaying);
+        }}
+        name="play"
+        size={35}
+        color="black"
+        style={styles.playIconStyle}
+      />
+    );
 
-        const recordConst=<RecIcon onPress={()=>{
-            if(this.state.isRecording===true){
-                this.setState({isRecording:false});
-            }else
-            {
-                this.setState({isRecording:true});
-            }
-        }}name='record' size={35} color='black' style={styles.recIconStyle} />;
-        const saveRecordConst=<RecIconForPlaying onPress={()=>{
-            if(this.state.isRecording===true){
-                this.setState({isRecording:false});
-            }else
-            {
-                this.setState({isRecording:true});
-            }
-        }} name='record' size={35} color='red' style={styles.recIconStyle} />;
+    const recordConst = (
+      <RecIcon
+        onPress={() => {
+          if ((this.state.isRecording === true)) {
+            this.setState({ isRecording: false });
+          } else {
+            this.setState({ isRecording: true });
+          }
+        }}
+        name="record"
+        size={35}
+        color="black"
+        style={styles.recIconStyle}
+      />
+    );
+    const saveRecordConst = (
+      <RecIconForPlaying
+        onPress={() => {
+          if (this.state.isRecording === true) {
+            this.setState({ isRecording: false });
+          } else {
+            this.setState({ isRecording: true });
+          }
+        }}
+        name="record"
+        size={35}
+        color="red"
+        style={styles.recIconStyle}
+      />
+    );
 
+    return (
+      <View style={styles.buttonContainer}>
+        <SliderComponent
+          //truyen currentSecs va duration vao
+          currentTime={this.state.currentTime}
+          durationTime={this.state.duration}
+          durationPlayerTimeInSecs={this.state.durationTimeInSecs}
+          currentTimePlayerInSecs={this.state.currentTimePlayerInSecs}
+        />
+        <View style={styles.playAndRecContainerStyle}>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            {this.state.isRecording ? saveRecordConst : recordConst}
+            <Text style={{ color: "black" }}>Record</Text>
+          </View>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            {/* {(this.state.isLoading&&this.state.isPlaying)?loaderConst:(this.state.isPlaying?pauseConst:playConst)} */}
+            {/* {this.state.isPlaying?pauseConst:playConst} */}
+            {this.state.isLoading ? loaderConst : playConst}
 
-        return(
-             <View style={styles.buttonContainer}>
-                             <SliderComponent/>
-                             <View style={styles.playAndRecContainerStyle}>
-            <View style={{justifyContent:'center',alignItems:'center'}}>
-                 {this.state.isRecording?saveRecordConst:recordConst}       
-                <Text style={{color:'black'}}>Record</Text>
-            </View>
-            <View style={{justifyContent:'center',alignItems:'center'}}>
-                {this.state.isPlaying?pauseConst:playConst}       
-                <Text style={{color:'black'}}>Play</Text>
-            </View>
-            <View style={{justifyContent:'center',alignItems:'center'}}>
-                <CheckIcon name='speaker-wireless' size={35}/>
-                <Text style={{color:'black'}}>Replay</Text>
-            </View>
+            {/* {playConst} */}
+            <Text style={{ color: "black" }}>Play</Text>
+          </View>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <CheckIcon name="speaker-wireless" size={35} />
+            <Text style={{ color: "black" }}>Replay</Text>
+          </View>
         </View>
+      </View>
 
-             </View>   
-         
-        //    {this.state.isReplaying?{color:'red'}:{color:'black'}}
-        );
-    }
+      //    {this.state.isReplaying?{color:'red'}:{color:'black'}}
+    );
+  }
 }
 
-export default class SpeakingDetailScreen extends Component{
-    componentDidMount(){
-        console.log(this.props.title);
-    }
-    constructor(args){
-        super(args);
-        this.state={
-            isPlaying:false,
-            isRecording:false,
-            isReplaying:false
-        }
+export default class SpeakingDetailScreen extends Component {
+  componentDidMount() {
+    console.log(this.props.title);
+  }
+  
+  constructor(args) {
+    super(args);
+  }
 
-    }
-    render(){
+  render() {
+    return (
+      <View style={styles.parentViewStyle}>
+        <Text style={styles.titleStyle}>{this.props.title}</Text>
+        <Text style={styles.lessonTextStyle} textBreakStrategy="highQuality">
+          {faker.lorem.paragraph(8)}
+        </Text>
+        <View style={styles.crossinglineStlye} />
+       
 
-        return(
-            <View style={styles.parentViewStyle}>
-            <Text style={styles.titleStyle}>
-            {this.props.title}
-            </Text>
-            <Text style={styles.lessonTextStyle}
-            textBreakStrategy='highQuality'
-            >
-                {faker.lorem.paragraph(10)}
-
-            </Text>
-            <View style={styles.crossinglineStlye}/>
-            <ButtonsComponent
-            />
-            </View>
-        );
-    }
-    
+        <ButtonsComponent />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    parentViewStyle:{
-        flex:1,
-        backgroundColor:'rgba(179, 255, 255,.3)'
-    },
-    titleStyle:{
-        fontSize:28,
-        fontWeight:'bold',
-        color:'black',
-        fontStyle:'italic',
-        alignSelf:'center',
-        margin:10,
-        textAlign:'center'
-    },
-    lessonTextStyle:{
-        fontWeight:'300',
-        fontSize:18,
-        color:'black',
-        marginLeft:3,
-        marginTop:15,
-    },
-    crossinglineStlye:{
-        flexDirection:'row',
-        height:1,
-        backgroundColor:'grey',
-        marginTop:30,
-        opacity:1
-        
-    },
-    playAndRecContainerStyle:{
-        flexDirection:'row',
-        flex:1,
-        alignItems:'center',
-        justifyContent:'space-around'
-
-    },
-    playIconStyle:{
-        
-    },
-    sliderStyle:{
-        height:50,
-        width:screenWidth-100,
-
-    },
-    sliderContainer:{
-        justifyContent:'center',
-        alignItems:'center',
-        flex:1,
-        flexDirection:'row'
-    }
-    ,
-    buttonContainer:{
-        flex:1
-    }
-    ,
-    childView:{
-        flex:1
-    },
-    currentPositionSecStyle:{
-        color:'black',
-        fontWeight:'bold',
-        fontSize:15
-    },
-    durationPlayerTime:{
-        color:'black',
-        fontWeight:'bold',
-        fontSize:15
-        }
-
+  parentViewStyle: {
+    flex: 1,
+    backgroundColor: "rgba(179, 255, 255,.3)"
+  },
+  titleStyle: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "black",
+    fontStyle: "italic",
+    alignSelf: "center",
+    margin: 10,
+    textAlign: "center"
+  },
+  lessonTextStyle: {
+    fontWeight: "300",
+    fontSize: 18,
+    color: "black",
+    marginLeft: 3,
+    marginTop: 15
+  },
+  crossinglineStlye: {
+    flexDirection: "row",
+    height: 1,
+    backgroundColor: "grey",
+    marginTop: 30,
+    opacity: 1
+  },
+  playAndRecContainerStyle: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  playIconStyle: {},
+  sliderStyle: {
+    height: 50,
+    width: screenWidth - 100
+  },
+  sliderContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    flexDirection: "row"
+  },
+  buttonContainer: {
+    flex: 1
+  },
+  childView: {
+    flex: 1
+  },
+  currentPositionSecStyle: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 15
+  },
+  durationPlayerTime: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 15
+  },
+  loaderStyle: {}
 });
